d8385826af7d4b69be4d7164fafb420792e7e38e
mbellanfonte <36000852+mbellanfonte@users.noreply.github.com>
2021-09-29 18:55:17 -0500

    Used multiple lines for Javadoc

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -4,7 +4,9 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
-/** This is the Calculator public class to perform calculations */
+/** 
+ * This is the Calculator public class to perform calculations 
+ */
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
