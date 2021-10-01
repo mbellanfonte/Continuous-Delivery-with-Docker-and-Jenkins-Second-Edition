851de815fba3ea16dc962d2c9ef34f5b4731efff
mbellanfonte <36000852+mbellanfonte@users.noreply.github.com>
2021-09-30 19:57:10 -0500

    Checkstyle error corrected

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,9 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/** 
+ * This is the Calculator public class to perform calculations 
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
