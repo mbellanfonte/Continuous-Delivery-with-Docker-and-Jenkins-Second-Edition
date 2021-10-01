d88c464185b0c2d6f4a51da84d172da833ec3a86
mbellanfonte <36000852+mbellanfonte@users.noreply.github.com>
2021-09-30 19:52:53 -0500

    checkstyle error re-inserted

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,9 +3,6 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/** 
- * This is the Calculator public class to perform calculations 
- */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
