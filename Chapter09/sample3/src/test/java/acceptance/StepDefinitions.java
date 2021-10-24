1df9bd79b6616cbb0bda6e016e66bbda61079441
mbellanfonte <36000852+mbellanfonte@users.noreply.github.com>
2021-10-24 00:18:32 -0500

    Update StepDefinitions.java

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -33,24 +33,7 @@ public class StepDefinitions {
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
     }
-}
-
-/** Steps definitions for calculator.feature - division*/
-public class StepDefinitions {
-    private String server = System.getProperty("calculator.url");
-
-    private RestTemplate restTemplate = new RestTemplate();
-
-    private String a;
-    private String b;
-    private String result;
-
-    @Given("^I have two numbers: (.*) and (.*)$")
-    public void i_have_two_numbers(String a, String b) throws Throwable {
-        this.a = a;
-        this.b = b;
-    }
-
+    
     @When("^the calculator divides them$")
     public void the_calculator_divides_them() throws Throwable {
         String url = String.format("%s/div?a=%s&b=%s", server, a, b);
